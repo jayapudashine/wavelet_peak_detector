36a9311d831c4cb8214f65a59e091da3263815cd
titusfranz <titus.franz@web.de>
2018-01-30 03:20:52 +0100

    up

--- tests/test_wavelet_peak_detector.py
+++ tests/test_wavelet_peak_detector.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from __future__ import print_function
+
 """Tests for `wavelet_peak_detector` package."""
 
 
 
 from wavelet_peak_detector import wavelet_peak_detector
 
-def test_hello:
+def test_hello():
     assert wavelet_peak_detector.main() == 'hello'
 
 
